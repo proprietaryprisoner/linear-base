b42f4eed50e1440a67916cc101922bc186a86d87
Felix Springer <felix.springer@possehl-analytics.com>
2022-06-29 14:09:52 +0200

    Add class `MonadTransUnlift`.

--- src/Control/Functor/Linear/Internal/MonadTrans.hs
+++ src/Control/Functor/Linear/Internal/MonadTrans.hs
@@ -6,6 +6,7 @@
 
 module Control.Functor.Linear.Internal.MonadTrans
   ( MonadTrans (..),
+    MonadTransUnlift (..),
   )
 where
 
@@ -13,3 +14,6 @@ import Control.Functor.Linear.Internal.Class
 
 class (forall m. Monad m => Monad (t m)) => MonadTrans t where
   lift :: Monad m => m a %1 -> t m a
+
+class MonadTrans t => MonadTransUnlift t where
+  liftWithUnlift :: Monad m => ((forall x. t m x -> m x) -> m a) -> t m a
